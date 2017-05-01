826d79b9d907559cd9ad1024c0dfebe15cb25395
Pavel Drobushevich <p.drobushevich@gmail.com>
2017-05-01 12:36:01 +0200

    Fix scissors

--- RockPaperScissors.ino
+++ RockPaperScissors.ino
@@ -263,30 +263,33 @@ void make_papper() {
 //////////////////    SCISSORS    ///////////////////
 ////////////////////////////////////////////////////////
 void make_scissors() {
-  while(pos123 <= OBERES_LIMIT)
+  while( readyPos >= UNTERES_LIMIT )
   {
-    // ringfinger motors: 0, 1, 2, 3, 4 (pwm board)
-    // motor0: stretch, motor1: tilt left, motor2: finger flexion, motor3: fingertip flexion, motor4: tilt right
-    pwm.setPWM(0, 0, pos123);   
-    pwm.setPWM(1, 0, 600);  
-    pwm.setPWM(2, 0, pos123);
-    pwm.setPWM(3, 0, pos123);
-    pwm.setPWM(4, 0, 600);
+    // indexfinger motors: 10, 11, 12, 13, 14 (pwm board)
+    // motor10: stretch, motor11: tilt left, motor12: finger flexion, motor13: fingertip flexion, motor14: tilt right
+    pwm.setPWM(10, 0, readyPos);   
+    pwm.setPWM(11, 0, 600);  
+    pwm.setPWM(12, 0, readyPos);
+    pwm.setPWM(13, 0, readyPos);
+    pwm.setPWM(14, 0, 600);
 
-    // pinky motors: 5, 6, 7, 8, 9 (pwm2 board)
+    // middlefinger motors: 5, 6, 7, 8, 9 (pwm board)
     // motor5: stretch, motor6: tilt left, motor7: finger flexion, motor8: fingertip flexion, motor9: tilt right
-    pwm2.setPWM(5, 0, pos123);   
-    pwm2.setPWM(6, 0, 600);  
-    pwm2.setPWM(7, 0, pos123);
-    pwm2.setPWM(8, 0, pos123);
-    pwm2.setPWM(9, 0, 600);
-    
-    pos123+=SCHRITTWEITE;
+    pwm.setPWM(5, 0, readyPos);   
+    pwm.setPWM(6, 0, 600);  
+    pwm.setPWM(7, 0, readyPos);
+    pwm.setPWM(8, 0, readyPos);
+    pwm.setPWM(9, 0, 600);
+
+    Serial.println("Position beugen alle:");
+    Serial.println(pos123);
+    readyPos -= SCHRITTWEITE; 
   }
 }
 
 void make_turn() {
   randNumber = random(3);
+//randNumber = 2;
   Serial.println(randNumber);
   switch (randNumber) {
     case 0:
